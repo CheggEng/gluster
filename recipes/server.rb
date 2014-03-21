5f7f5cc5ab5e184669922da70e001b6b7b1db7fd
Richard Shade <rshade@rightscale.com>
2014-03-21 15:10:27 -0500

    adding value

--- recipes/server.rb
+++ recipes/server.rb
@@ -2,6 +2,13 @@ package 'glusterfs-server' do
   action :install
 end
 
-service "glusterfs-server" do
+service value_for_platform(
+  ["centos", "redhat", "fedora"] => {
+   "default" => "glusterd"
+  },
+  ["ubuntu", "debian"] => {
+   "default" => "glusterfs-server"
+  }
+) do
   action [ :enable, :start ]
 end
