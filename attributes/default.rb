bd6daff4e27dd7a12aff2a4a49381f52e24da746
Richard Shade <rshade@rightscale.com>
2014-03-21 16:35:44 -0500

    starting volume opts

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,3 +1,9 @@
 default[:gluster][:peers]=Hash.new
 default[:gluster][:brick][:path]="/mnt"
 default[:gluster][:client][:mount]="/mnt/gluster"
+
+#gluster volume options
+default[:gluster][:volume][:options]=Hash.new
+default[:gluster][:volume][:options]['auth.allow']='*'
+default[:gluster][:volume][:options]['nfs.disable']='on'
+
