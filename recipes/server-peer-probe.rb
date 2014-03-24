ea37cfea4f38d465777984aee9e1677c707b3e2e
Richard Shade <rshade@rightscale.com>
2014-03-24 15:46:03 -0500

    moving peered variable up

--- recipes/server-peer-probe.rb
+++ recipes/server-peer-probe.rb
@@ -7,10 +7,12 @@ node[:gluster][:peers].each do |peer|
     config = XmlSimple.xml_in(Mixlib::ShellOut.new("gluster peer status --xml").run_command.stdout)
     if config["opErrstr"][0]=="peer status: No peers present" 
       peer_probe=Mixlib::ShellOut.new("gluster peer probe #{peer}").run_command
-      Chef::Log.info "gluster peer probe #{peer} STDOUT:#{peer_probe.stdout}, STDERR:#{peer_probe.stderr}"
+      Chef::Log.info "gluster peer probe #{peer} STDOUT:\'#{peer_probe.stdout}\', STDERR:\'#{peer_probe.stderr}\'"
     else
+      peered=false
       config["peerStatus"][0]["peer"].each do |peered_host|
         if peered_host["hostname"] == peer
+          Chef::Log.info "Peer already logged"
           peered = true
         end
       end
