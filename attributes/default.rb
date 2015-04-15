bbbb1d306c4d14cebbe955865c8d90d810cd4372
Richard Shade <rshade@rightscale.com>
2015-04-15 16:38:35 -0500

    fixing FC001 in attributes/default.rb

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,12 +1,11 @@
-default[:gluster][:peers]=Array.new
-default[:gluster][:brick][:path]="/mnt"
-default[:gluster][:client][:mount]="/mnt/gluster"
+default['gluster']['peers'] = Array.new
+default['gluster']['brick']['path'] = "/mnt"
+default['gluster']['client']['mount'] = "/mnt/gluster"
 
 #gluster volume options
-default[:gluster][:volume][:name]='gluster-volume'
-default[:gluster][:volume][:options]=Hash.new
-default[:gluster][:volume][:options]['auth.allow']='*'
-default[:gluster][:volume][:options]['nfs.disable']='off'
-default[:gluster][:volume][:options]['network.frame-timeout']='60'
-default[:gluster][:volume][:options]['performance.cache-size']='32MB'
-
+default['gluster']['volume']['name'] = 'gluster-volume'
+default['gluster']['volume']['options'] = Hash.new
+default['gluster']['volume']['options']['auth.allow'] = '*'
+default['gluster']['volume']['options']['nfs.disable'] = 'off'
+default['gluster']['volume']['options']['network.frame-timeout'] = '60'
+default['gluster']['volume']['options']['performance.cache-size'] = '32MB'
