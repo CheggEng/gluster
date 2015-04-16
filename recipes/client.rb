70656f04aa9167cb1bff80fdc78ef0a8084aa3b3
Richard Shade <rshade@rightscale.com>
2015-04-16 15:06:26 -0500

    adding directory and client mount, and tests

---
+++ recipes/client.rb
@@ -0,0 +1,22 @@
+package "glusterfs" do
+  action :install
+end
+
+package "glusterfs-fuse" do
+  action :install
+end
+
+directory node['gluster']['client']['mount']['point'] do
+  owner "root"
+  group "root"
+  mode "0755"
+  recursive true
+  action :create
+end
+
+mount node['gluster']['client']['mount']['point'] do
+  fstype "glusterfs"
+  device "#{node['gluster']['peers'].first}:#{node['gluster']['brick']['path']}"
+  options node['gluster']['client']['mount']['options']
+  action   [:mount, :enable]
+end
--- spec/client_spec.rb
+++ spec/client_spec.rb
@@ -1,7 +1,16 @@
 require_relative 'spec_helper'
 
 describe 'gluster::client' do
-  let(:chef_run) { ChefSpec::Runner.new.converge(described_recipe) }
+  let(:chef_run) do
+    ChefSpec::Runner.new do |node|
+      node.set[:gluster][:peers] = ['10.0.0.1']
+      node.set['gluster']['brick']['path'] = "/mnt"
+      node.set['gluster']['client']['mount']['point'] = '/mnt/gluster'
+      node.set['gluster']['client']['mount']['options'] = []
+    end.converge(described_recipe)
+  end
+  let(:mount_point) { chef_run.node['gluster']['client']['mount']['point'] }
+  let(:mount_options) { chef_run.node['gluster']['client']['mount']['options'] }
 
   it 'installs client package' do
     expect(chef_run).to install_package('glusterfs')
@@ -9,5 +18,23 @@ describe 'gluster::client' do
 
   it 'installs client fuse package' do
     expect(chef_run).to install_package('glusterfs-fuse')
+  end
+
+  it 'creates the mount point' do
+    expect(chef_run).to create_directory(mount_point).with(
+      owner: "root",
+      group: "root",
+      mode: "0755",
+      recursive: true
+    )
+  end
+
+  it 'mounts a gluster volume' do
+    expect(chef_run).to mount_mount(mount_point).with(
+      fstype: "glusterfs",
+      device: "#{chef_run.node['gluster']['peers'].first}:#{chef_run.node['gluster']['brick']['path']}",
+      options: mount_options
+    )
+    expect(chef_run).to enable_mount(mount_point)
   end
 end
