11328f62e3d1d6a26b2003cde7eed27d9dcac3d7
Richard Shade <rshade@rightscale.com>
2015-08-24 15:15:38 -0500

    adding start to replica

--- recipes/setup-replica.rb
+++ recipes/setup-replica.rb
@@ -1,17 +1,34 @@
-require 'mixlib/shellout'
-require 'xmlsimple'
 
-volume_name=node['gluster']['volume']['name']
-peers=node['gluster']['peers']
-Chef::Log.info "Peer Count: #{peers.size.to_s}"
-cmd = "gluster volume create #{volume_name} replica #{peers.size.to_s} "
+ruby_block "creating replica" do
+  block do
+    require 'mixlib/shellout'
+    require 'xmlsimple'
 
-peers.each do |peer|
-  cmd += "#{peer}:#{node['gluster']['brick']['path']} "
+    volume_name=node['gluster']['volume']['name']
+    peers=node['gluster']['peers']
+    Chef::Log.info "Peer Count: #{peers.size.to_s}"
+    cmd = "gluster volume create #{volume_name} replica #{peers.size.to_s} "
+
+    peers.each do |peer|
+      cmd += "#{peer}:#{node['gluster']['brick']['path']} "
+    end
+    Chef::Log.info "Adding force command to mount root partition"
+    cmd += "force"
+
+    Chef::Log.info "Running: #{cmd}"
+    brick_create=Mixlib::ShellOut.new(cmd).run_command
+    Chef::Log.info "Status: STDOUT: #{brick_create.stdout}, STDERR: #{brick_create.stderr}"
+  end
 end
-Chef::Log.info "Adding force command to mount root partition"
-cmd += "force"
 
-Chef::Log.info "Running: #{cmd}"
-brick_create=Mixlib::ShellOut.new(cmd).run_command
-Chef::Log.info "Status: STDOUT: #{brick_create.stdout}, STDERR: #{brick_create.stderr}"
+ruby_block "starting replica" do
+  block do
+    require 'mixlib/shellout'
+    require 'xmlsimple'
+
+    volume_name=node['gluster']['volume']['name']
+    cmd = "gluster volume start #{volume_name}"
+    volume_start = Mixlib::ShellOut.new(cmd).run_command
+    Chef::Log.info "Status: STDOUT: #{volume_start.stdout}, STDERR: #{volume_start.stderr}"
+  end
+end
